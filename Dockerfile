ab29e9f7363303aaecea62fe838c790a6b97ed2d
lemonc <lvmengcheng527@163.com>
2025-06-04 21:48:15 +0800

    删除不必要的config配置

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,9 @@ FROM alpine:latest
 WORKDIR /app
 COPY renamer .
 COPY config/config.yml ./config/
+COPY dist ./dist
+
+VOLUME /home
+
 EXPOSE 7777
 ENTRYPOINT [ "./renamer" ]
--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"github.com/lemonc7/renamer/config"
 	"github.com/lemonc7/renamer/router"
 )
 
 func main() {
-	config.InitConfig()
 	app := router.SetupRouter()
-	app.Logger.Fatal(app.Start(":"+config.Server.Port))
+	app.Logger.Fatal(app.Start(":7777"))
 }
