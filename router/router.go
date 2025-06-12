7b103cbe218f8265dc83a1ae53316b697802c5bd
lemonc <lvmengcheng527@163.com>
2025-06-12 22:09:24 +0800

    使用新的日志中间件，直接使用日志打印错误，全局错误处理只返回error响应

--- router/router.go
+++ router/router.go
@@ -1,7 +1,6 @@
 package router
 
 import (
-	"bytes"
 	"fmt"
 	"net/http"
 
@@ -16,57 +15,57 @@ import (
 func SetupRouter() *echo.Echo {
 	app := echo.New()
 
-	// 全局处理错误
-	app.HTTPErrorHandler = func(err error, c echo.Context) {
-		// 默认错误码
-		code := http.StatusInternalServerError
-		var msg any
-		// 判断err类型,转化为字符串
-		switch he := err.(type) {
-		case *echo.HTTPError:
-			code = he.Code
-			msg = fmt.Sprintf("%v", he.Message)
-		default:
-			msg = err.Error()
-		}
-
-		// 打印error到终端
-		if code >= 500 {
-			fmt.Printf("\033[31m[ERROR]\033[0m %v\n", msg)
-		} else {
-			fmt.Printf("\033[33m[WARN] \033[0m %v\n", msg)
-		}
-		
-		// 返回响应
-		c.JSON(code, echo.Map{
-			"error": msg,
-		})
-	}
-
-	// 跨域
-	app.Use(middleware.CORS())
 	// 异常恢复
 	app.Use(middleware.Recover())
 
+	// 跨域
+	app.Use(middleware.CORS())
+
 	// 日志打印
-	app.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-		Format:           "${custom} ${time_custom} | ${status} | ${remote_ip} | ${latency_human} | ${method} ${uri}\n",
-		CustomTimeFormat: "2006/01/02 - 15:04:05",
-		CustomTagFunc: func(c echo.Context, buf *bytes.Buffer) (int, error) {
-			status := c.Response().Status
-			
-			switch {
-			case status >= 500:
-				// 服务器错误
-				return buf.WriteString("\033[31m[ERROR]\033[0m")
-			case status >= 400:
-				// 客户端错误
-				return buf.WriteString("\033[33m[WARN] \033[0m")
-			default:
-				return buf.WriteString("\033[32m[INFO] \033[0m")
+	app.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogStatus:   true,
+		LogURI:      true,
+		LogRemoteIP: true,
+		LogLatency:  true,
+		LogMethod:   true,
+		LogError:    true,
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			fmt.Printf("[INFO]  %v | %v | %-15v | %12v | %-6v %v\n",
+				v.StartTime.Format("2006/01/02 - 15:04:05"),
+				v.Status,
+				v.RemoteIP,
+				v.Latency,
+				v.Method,
+				v.URI,
+			)
+
+			_, errMsg := parseError(v.Error)
+
+			// 打印error到终端
+			if v.Status >= 500 {
+				fmt.Printf("\033[31m[ERROR]\033[0m %s\n", errMsg)
+			} else if v.Status >= 400 {
+				fmt.Printf("\033[33m[WARN] \033[0m %s\n", errMsg)
 			}
+
+			return nil
 		},
 	}))
+
+	// 全局处理错误
+	app.HTTPErrorHandler = func(err error, c echo.Context) {
+		// 如果已经响应客户端,就不做处理
+		if c.Response().Committed {
+			return
+		}
+
+		code, errMsg := parseError(err)
+
+		// 返回响应
+		c.JSON(code, echo.Map{
+			"error": errMsg,
+		})
+	}
 
 	// 注册验证参数的方法
 	validate := validator.New()
@@ -74,7 +73,9 @@ func SetupRouter() *echo.Echo {
 
 	// 路由设置
 	app.GET("/ping", func(c echo.Context) error {
-		return c.String(http.StatusOK, "pong")
+		return c.JSON(http.StatusOK, echo.Map{
+			"message": "pong",
+		})
 	})
 	api := app.Group("/api/files")
 	api.GET("", controller.GetFiles)
@@ -96,4 +97,32 @@ func SetupRouter() *echo.Echo {
 	})
 
 	return app
+}
+
+// 处理错误信息
+func parseError(err error) (code int, msg string) {
+	if err == nil {
+		return http.StatusOK, ""
+	}
+
+	// 处理*echo.HTTPError错误
+	if he, ok := err.(*echo.HTTPError); ok {
+		code = he.Code
+		// 判断Message的错误类型
+		switch msgType := he.Message.(type) {
+		case error:
+			msg = msgType.Error()
+		case string:
+			msg = msgType
+		default:
+			msg = fmt.Sprintf("%v", msgType)
+		}
+		return code, msg
+	}
+
+	// 处理标准error
+	code = http.StatusInternalServerError
+	msg = err.Error()
+	return code, msg
+
 }
